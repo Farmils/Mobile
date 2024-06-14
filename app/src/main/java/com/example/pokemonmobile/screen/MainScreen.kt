ac3d87141aabb88e7fcdbd1324c6cb8de0fea5cc
EugenIsay <zheka4311@gmail,com>
2024-06-14 18:54:05 +0300

    full header

--- app/src/main/java/com/example/pokemonmobile/screen/MainScreen.kt
+++ app/src/main/java/com/example/pokemonmobile/screen/MainScreen.kt
@@ -1,22 +1,26 @@
 package com.example.pokemonmobile.screen
 
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuInflater
 import android.view.View
-import android.widget.Button
+import android.widget.ImageButton
+import androidx.annotation.AttrRes
+import androidx.annotation.StyleRes
+import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
 import com.example.pokemonmobile.R
 
 
 class MainScreen : Fragment(R.layout.fragment_main_screen) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val button=view.findViewById<Button>(R.id.NavigateButton)
-        button.setOnClickListener {
-            parentFragmentManager.commit {
-                replace<AboutScreen>(R.id.host_fragment)
-            }
+        val changeMode = view.findViewById<ImageButton>(R.id.changeMode)
+        changeMode.setOnClickListener{
+            val menu = PopupMenu(requireContext(),it, )
+            val inflater: MenuInflater = menu.menuInflater
+            inflater.inflate(R.menu.search_menu,menu.menu)
+            menu.show()
         }
     }
 
